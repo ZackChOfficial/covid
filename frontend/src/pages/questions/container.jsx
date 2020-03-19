d0f438b42ef708701e5b46468249fc262efa38f2
ayoubayt <ayoub.aguert@gmail.com>
2020-03-19 20:03:28 +0100

    ok

--- frontend/src/pages/questions/container.jsx
+++ frontend/src/pages/questions/container.jsx
@@ -4,7 +4,6 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import questionsData from "./data";
 
 const useStyles = makeStyles(theme => ({
