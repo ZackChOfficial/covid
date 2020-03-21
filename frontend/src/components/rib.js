1b0c61b094de42db05ba62d844a81d0f138bb339
zack ch <zack.ch.official@gmail.com>
2020-03-22 00:34:24 +0100

    add link

--- frontend/src/components/rib.js
+++ frontend/src/components/rib.js
@@ -4,8 +4,8 @@ import styles from "./css/rib.module.css";
 export default function Rib() {
   return (
     <div className={styles.rib}>
-      المساهمة في الصندوق الخاص بتدبير جائحة فيروس كورونا :
-      <bold> 001 810 00 780 002 011 062 02 21</bold>
+      001 810 00 780 002 011 062 02 :
+      <span> المساهمة في الصندوق الخاص بتدبير جائحة فيروس كورونا</span>{" "}
     </div>
   );
 }
